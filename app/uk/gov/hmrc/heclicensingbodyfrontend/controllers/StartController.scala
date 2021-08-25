f72c1d0b02895d0f66afe79510758b78dcd98d36
AndyHWChung <26929776+AndyHWChung@users.noreply.github.com>
2021-08-25 16:46:21 +0100

    fix compile error

--- app/uk/gov/hmrc/heclicensingbodyfrontend/controllers/StartController.scala
+++ app/uk/gov/hmrc/heclicensingbodyfrontend/controllers/StartController.scala
@@ -26,7 +26,7 @@ import uk.gov.hmrc.heclicensingbodyfrontend.util.Logging
 import uk.gov.hmrc.heclicensingbodyfrontend.util.Logging.LoggerOps
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class StartController @Inject() (
