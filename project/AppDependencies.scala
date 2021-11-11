1d07d3d367f15927fa4b01e5022623b470bf4ff8
priyankadhiman3006 <88194246+priyankadhiman3006@users.noreply.github.com>
2021-11-11 12:33:19 +0000

    HEC-1398 upgrade lib

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,9 +4,9 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.14.0",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "0.55.0",
-    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "1.14.0-play-28",
+    "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % "5.16.0",
+    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % "0.56.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "1.25.0-play-28",
     "org.typelevel"     %% "cats-core"                  % "2.6.1",
     "ai.x"              %% "play-json-extensions"       % "0.42.0",
     "com.github.kxbmap" %% "configs"                    % "0.4.4"
