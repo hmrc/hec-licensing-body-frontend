c2a37d365732c0af3654b4249dbaf607f5767c7c
Priyanka Dhiman <88194246+priyankadhiman3006@users.noreply.github.com>
2022-01-12 15:15:12 +0000

    lib upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -8,7 +8,7 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-28" % bootStrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28"         % hmrcMongoVersion,
     "uk.gov.hmrc"       %% "play-frontend-hmrc"         % "1.31.0-play-28",
-    "org.typelevel"     %% "cats-core"                  % "2.6.1",
+    "org.typelevel"     %% "cats-core"                  % "2.7.0",
     "ai.x"              %% "play-json-extensions"       % "0.42.0",
     "com.github.kxbmap" %% "configs"                    % "0.6.1"
   )
@@ -18,7 +18,7 @@ object AppDependencies {
     "org.scalatest"          %% "scalatest"               % "3.2.10"            % Test,
     "org.jsoup"               % "jsoup"                   % "1.14.3"            % Test,
     "com.typesafe.play"      %% "play-test"               % PlayVersion.current % Test,
-    "org.scalamock"          %% "scalamock"               % "4.2.0"             % Test,
+    "org.scalamock"          %% "scalamock"               % "5.2.0"             % Test,
     "uk.gov.hmrc.mongo"      %% "hmrc-mongo-test-play-28" % hmrcMongoVersion    % Test,
     "com.vladsch.flexmark"    % "flexmark-all"            % "0.62.2"            % "test, it",
     "org.scalatestplus.play" %% "scalatestplus-play"      % "5.1.0"             % "test, it"
