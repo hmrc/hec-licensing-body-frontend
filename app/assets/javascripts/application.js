d3d229fb3ca1c046a205023bf56532537a115199
Alistair Macdonald <1460971+nubz@users.noreply.github.com>
2022-05-13 10:00:14 +0100

    HEC-2117 add polyfil (#56)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,4 +1,11 @@
 (function (document, window) {
+  if ('NodeList' in window && !NodeList.prototype.forEach) {
+    NodeList.prototype.forEach = function (callback, scope) {
+      for (let i = 0; i < this.length; i++) {
+        callback.call(scope || window, this[i], i, this)
+      }
+    }
+  }
   document.querySelectorAll('a[href="#print-dialogue"]')
     .forEach(function(link) {
       link.addEventListener('click', function(event) {
