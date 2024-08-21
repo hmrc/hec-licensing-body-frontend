cb8b880503a9d19cc2e28fddec2d1c9c036093b7
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2024-08-21 12:53:02 +0100

    Using new message variable rather than static text in unit tests.

--- test/uk/gov/hmrc/heclicensingbodyfrontend/controllers/CRNControllerSpec.scala
+++ test/uk/gov/hmrc/heclicensingbodyfrontend/controllers/CRNControllerSpec.scala
@@ -183,9 +183,7 @@ class CRNControllerSpec
               button.attr("action") shouldBe routes.CRNController.companyRegistrationNumberSubmit.url
 
               val link = doc.select("p > .govuk-link")
-              link.text should startWith(
-                "search Companies House for your company registration number (opens in new tab)"
-              )
+              link.text should startWith(messageFromMessageKey("crn.linkMessage"))
             }
           )
         }
@@ -219,9 +217,7 @@ class CRNControllerSpec
               button.attr("action") shouldBe routes.CRNController.companyRegistrationNumberSubmit.url
 
               val link = doc.select("p > .govuk-link")
-              link.text should startWith(
-                "search Companies House for your company registration number (opens in new tab)"
-              )
+              link.text should startWith(messageFromMessageKey("crn.linkMessage"))
 
               val input = doc.select(".govuk-input")
               input.attr("value") shouldBe validCRN(0).value
