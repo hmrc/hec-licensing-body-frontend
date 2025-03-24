b5191484aed59bf13b8f5ffa1bcbd4ea3fe23c9d
Athish Thayalan <120419238+AthishThayalan@users.noreply.github.com>
2025-03-24 10:03:38 +0000

    DDCYLS-6429 - minor dependency upgrades (#93)
    * minor dependency upgrades
    
    * increase scala version
    
    * upgraded kind-projector dependency
    
    * revert scala version
    
    * upgrade scala
    
    * upgraded scoverage

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,13 +1,13 @@
 import sbt._
 
 object AppDependencies {
-  val bootStrapVersion = "9.7.0"
-  val hmrcMongoVersion = "2.4.0"
+  val bootStrapVersion = "9.11.0"
+  val hmrcMongoVersion = "2.6.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"         % bootStrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                 % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "11.11.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "12.0.0",
     "org.typelevel"     %% "cats-core"                          % "2.10.0"
   )
 
