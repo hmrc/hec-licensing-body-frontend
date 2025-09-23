8b6ab8780df2049432211d5105c8914e4f56b5f4
Bon Parker <45689313+bonparker@users.noreply.github.com>
2025-09-23 11:12:56 +0100

    Ddcyls 7153 (#104)
    * DDCYLS-7153: GOV.UK Rebrand and ‘play-frontend-hmrc’ upgrade
    
    * DDCYLS-7153: Amended CSS
    
    * DDCYLS-7153: Added semi colon

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -7,7 +7,7 @@ object AppDependencies {
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"         % bootStrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                 % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "12.7.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "12.8.0",
     "org.typelevel"     %% "cats-core"                          % "2.10.0"
   )
 
