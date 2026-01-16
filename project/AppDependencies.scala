c751cfb28750ff99f3d7610688d0403e5c416498
bharti.verma <68339135+bharti5verma@users.noreply.github.com>
2026-01-16 16:56:17 +0000

    DDCYLS-7707 Library upgraded

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,13 +1,13 @@
 import sbt._
 
 object AppDependencies {
-  val bootStrapVersion = "10.4.0"
+  val bootStrapVersion = "10.5.0"
   val hmrcMongoVersion = "2.11.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"         % bootStrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                 % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "12.25.0",
+    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "12.26.0",
     "org.typelevel"     %% "cats-core"                          % "2.13.0"
   )
 
