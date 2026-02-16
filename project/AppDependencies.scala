1aa9e0110b46cac32b66b12f63212ad2de5c2532
Liam-Macarthur <113617571+Liam-Macarthur@users.noreply.github.com>
2026-02-16 09:04:24 +0000

    DDCYLS 8070 - upgrading dependency (#110)
    * upgrading dependency
    
    * removing unused dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,10 +5,13 @@ object AppDependencies {
   val hmrcMongoVersion = "2.12.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc"       %% "bootstrap-frontend-play-30"         % bootStrapVersion,
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-30"                 % hmrcMongoVersion,
-    "uk.gov.hmrc"       %% "play-frontend-hmrc-play-30"         % "12.31.0",
-    "org.typelevel"     %% "cats-core"                          % "2.13.0"
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-30"         % bootStrapVersion,
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-30"                 % hmrcMongoVersion,
+    "uk.gov.hmrc"             %% "play-frontend-hmrc-play-30"         % "12.31.0",
+    "org.typelevel"           %% "cats-core"                          % "2.13.0",
+    "org.apache.commons"      % "commons-lang3"                       % "3.18.0",
+    "ch.qos.logback"          % "logback-core"                        % "1.5.21",
+    "at.yawk.lz4"             %  "lz4-java"                           % "1.10.3"
   )
 
   val test: Seq[ModuleID] = Seq(
